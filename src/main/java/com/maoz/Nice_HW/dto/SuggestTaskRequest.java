03d6f871bcdf5b155f3c5c1986484875a42f6395
Maoz1005 <maozhaim@mail.tau.ac.il>
2025-09-08 18:10:25 +0300

    DTOs and ISO8601 annotation

--- src/main/java/com/maoz/Nice_HW/dto/SuggestTaskRequest.java
+++ src/main/java/com/maoz/Nice_HW/dto/SuggestTaskRequest.java
@@ -2,16 +2,18 @@ package com.maoz.Nice_HW.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import com.maoz.Nice_HW.validation.Iso8601;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 import static com.maoz.Nice_HW.config.Constants.MAX_UTTERANCE_LENGTH;
 
 public record SuggestTaskRequest(
         @NotBlank(message = "utterance must not be blank")
-        @Size(max = MAX_UTTERANCE_LENGTH)
         String utterance,
 
         @NotBlank(message = "userId must not be blank")
+        @Size(min = 1, max = 9, message = "userId length must be between 1 and 9 digits")
+        @Pattern(regexp = "^[0-9]+$", message = "userId must contain only digits")
         String userId,
 
         @NotBlank(message = "sessionId must not be blank")
