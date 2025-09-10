bb4e09d3e3220677b4df09f6dc1a30686bebf7a8
Maoz1005 <maozhaim@mail.tau.ac.il>
2025-09-11 00:11:20 +0300

    config - final version

--- src/main/java/com/maoz/Nice_HW/config/Constants.java
+++ src/main/java/com/maoz/Nice_HW/config/Constants.java
@@ -2,6 +2,6 @@ package com.maoz.Nice_HW.config;
 
 public class Constants {
     public static final int MAX_UTTERANCE_LENGTH = 1000;
-    public static final double CLASSIFIER_THRESHOLD = 0.75;
+    public static final double CLASSIFIER_THRESHOLD = 0.72;
     public static final String NO_TASK_FOUND = "NoTaskFound";
 }
