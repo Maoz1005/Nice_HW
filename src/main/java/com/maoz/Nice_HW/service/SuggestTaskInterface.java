16777a4a870c4db412ded05489f559ab6eb1f269
Maoz1005 <maozhaim@mail.tau.ac.il>
2025-09-10 18:35:01 +0300

    SuggestTaskInterface - final version

--- src/main/java/com/maoz/Nice_HW/service/SuggestTaskInterface.java
+++ src/main/java/com/maoz/Nice_HW/service/SuggestTaskInterface.java
@@ -1,8 +1,11 @@
 package com.maoz.Nice_HW.service;
 
+import com.maoz.Nice_HW.config.TaskDictionary;
+
 /***
  * Interface for suggest task services.
  */
 public interface SuggestTaskInterface {
     public String suggestTask(String utterance);
+    public void updateDictionary(TaskDictionary taskDictionary);
 }
